ed1d10b98999281c44c299d08fe708db64437dfc
Hyvinin <hyvinin@gmail.com>
2026-09-10 10:40:35 -0300

    primeiro

---
+++ BhaskaraApp/Bhaskara.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BhaskaraApp
+{
+    public class Bhaskara
+    {
+        // Coeficientes da equação de segundo grau
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        // Construtor
+        public Bhaskara(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                throw new ArgumentException("O coeficiente A não pode ser zero.");
+            }
+
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        // Calculando o valor de Delta
+        private double CalcularDelta()
+        {
+            return (B * B) - (4 * A * C);
+        }
+
+        // Verificando se a equação possui raízes reais
+        public bool TemRaizesReais()
+        {
+            double delta = CalcularDelta();
+
+            return delta >= 0;
+        }
+
+        // Calculando as raízes da equação
+        public (double?, double?) CalcularRaizes()
+        {
+            if (!TemRaizesReais())
+            {
+                // Não possui raízes reais
+                return (null, null);
+            }
+
+            double delta = CalcularDelta();
+            double raizDelta = Math.Sqrt(delta);
+
+            // Fórmula de Bhaskara
+            double x1 = (-B + raizDelta) / (2 * A);
+            double x2 = (-B - raizDelta) / (2 * A);
+
+            return (x1, x2);
+        }
+    }
+}
---
+++ BhaskaraTest/BhaskaraTest.cs
@@ -0,0 +1,97 @@
+﻿using BhaskaraApp;
+
+namespace BhaskaraAppUnitTest
+{
+    [TestClass]
+    public sealed class BhaskaraTest
+    {
+        private Bhaskara calc;
+
+        [TestInitialize]
+        public void Inicializar()
+        {
+            // Criando instância para a propriedade calc
+            calc = new Bhaskara(1, -5, 6);
+        }
+
+        /* -------- Teste de condição normal -------- */
+
+        [TestMethod]
+        public void recebeParametros_DeveRetornarRaizesReais()
+        {
+            // Act - Invoca o artefato a ser testado
+            bool obtido = calc.TemRaizesReais();
+
+            // Assert - Compara os valores
+            Assert.IsTrue(obtido);
+        }
+
+        /* -------- Teste do cálculo das raízes -------- */
+
+        [TestMethod]
+        public void recebeParametros_DeveCalcularRaizes()
+        {
+            // Act - Invoca o artefato a ser testado
+            var obtido = calc.CalcularRaizes();
+
+            // Assert - Compara os valores
+            Assert.AreEqual(3, obtido.Item1);
+            Assert.AreEqual(2, obtido.Item2);
+        }
+
+        /* -------- Teste de Delta igual a zero -------- */
+
+        [TestMethod]
+        public void deltaZero_DeveRetornarUmaRaizReal()
+        {
+            // Arrange
+            calc = new Bhaskara(1, -4, 4);
+
+            // Act
+            bool obtido = calc.TemRaizesReais();
+
+            // Assert
+            Assert.IsTrue(obtido);
+        }
+
+        /* -------- Teste de Delta negativo -------- */
+
+        [TestMethod]
+        public void deltaNegativo_DeveRetornarFalse()
+        {
+            // Arrange
+            calc = new Bhaskara(1, 2, 5);
+
+            // Act
+            bool obtido = calc.TemRaizesReais();
+
+            // Assert
+            Assert.IsFalse(obtido);
+        }
+
+        /* -------- Teste de raízes nulas -------- */
+
+        [TestMethod]
+        public void deltaNegativo_DeveRetornarRaizesNulas()
+        {
+            // Arrange
+            calc = new Bhaskara(1, 2, 5);
+
+            // Act
+            var obtido = calc.CalcularRaizes();
+
+            // Assert
+            Assert.IsNull(obtido.Item1);
+            Assert.IsNull(obtido.Item2);
+        }
+
+        /* -------- Teste de A igual a zero -------- */
+
+        [TestMethod]
+        public void recebeAZero_DeveLancarArgumentException()
+        {
+            // Act + Assert
+            Assert.ThrowsExactly<ArgumentException>(() => new Bhaskara(0, 5, 6));
+        }
+    }
+}
